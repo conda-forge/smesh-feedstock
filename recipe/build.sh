b4bcb8a2fa4b18742f77b80915535a135aedb1a5
looooo <sppedflyer@gmail.com>
2018-05-15 12:36:03 +0200

    osx: circle-ci

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,13 @@
 mkdir -p build
 cd build
 
+
+if [ `uname` = "Darwin" ]; then
+	BUILD_WITH_NETGEN=OFF
+else
+	BUILD_WITH_NETGEN=ON
+fi
+
 cmake -G "Ninja" \
       -D CMAKE_INSTALL_PREFIX=$PREFIX \
       -D CMAKE_BUILD_TYPE=Release \
@@ -8,6 +15,7 @@ cmake -G "Ninja" \
       -D CMAKE_SYSTEM_PREFIX_PATH=$PREFIX \
       -D CMAKE_CXX_FLAGS="-std=c++11" \
       -D ENABLE_MED=OFF \
+      -D ENABLE_NETGEN:BOOL=$BUILD_WITH_NETGEN \
       ..
 
 ninja install
