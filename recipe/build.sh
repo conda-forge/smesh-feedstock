322a5d12310236299b7529d82653eb35a58d363a
lo <sppedflyer@gmail.com>
2018-07-08 18:30:21 +0200

    osx: try building with netgen support

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,16 +1,6 @@
 mkdir -p build
 cd build
 
-if [ `uname` = "Darwin" ]; then
-    sed -i '' 's/Xcode-9.app/Xcode.app/g' $PREFIX/lib/cmake/opencascade/OpenCASCADEVisualizationTargets.cmake
-fi
-
-if [ `uname` = "Darwin" ]; then
-	BUILD_WITH_NETGEN=OFF
-else
-	BUILD_WITH_NETGEN=ON
-fi
-
 cmake -G "Ninja" \
       -D CMAKE_INSTALL_PREFIX:FILEPATH=$PREFIX \
       -D CMAKE_BUILD_TYPE=Release \
@@ -18,7 +8,7 @@ cmake -G "Ninja" \
       -D CMAKE_SYSTEM_PREFIX_PATH:FILEPATH=$PREFIX \
       -D CMAKE_CXX_FLAGS="-std=c++11" \
       -D ENABLE_MED:BOOL=OFF \
-      -D ENABLE_NETGEN:BOOL=$BUILD_WITH_NETGEN \
+      -D ENABLE_NETGEN:BOOL=ON \
       ..
 
 ninja install
