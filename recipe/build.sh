4186361fdedbdc333c43e48f3f50bbbe4b2ddac7
looooo <sppedflyer@gmail.com>
2019-01-28 20:56:15 +0100

    use vtk 8.1.2

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,20 +1,14 @@
 mkdir -p build
 cd build
 
-if [ `uname` = "Darwin" ]; then
-	FLAGS="-std=c++14"
-else
-	FLAGS="-std=c++11"
-fi
-
 cmake -G "Ninja" \
       -D CMAKE_INSTALL_PREFIX:FILEPATH=$PREFIX \
       -D CMAKE_BUILD_TYPE=Release \
       -D CMAKE_PREFIX_PATH:FILEPATH=$PREFIX \
       -D CMAKE_SYSTEM_PREFIX_PATH:FILEPATH=$PREFIX \
-      -D CMAKE_CXX_FLAGS=$FLAGS \
+      -D CMAKE_CXX_STANDARD=14 \
       -D ENABLE_MED:BOOL=OFF \
       -D ENABLE_NETGEN:BOOL=ON \
       ..
 
-ninja install
+ninja install -v
