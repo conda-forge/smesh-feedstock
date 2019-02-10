842fa7459c29d19d31fc8ee302283b5bd696f396
lorenz <looooo@users.noreply.github.com>
2019-02-10 14:13:09 +0100

    use c++14 for osx

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,12 +1,18 @@
 mkdir -p build
 cd build
 
+if [ `uname` = "Darwin" ]; then
+      cpp_std=14
+else
+      cpp_std=17
+fi
+
 cmake -G "Ninja" \
       -D CMAKE_INSTALL_PREFIX:FILEPATH=$PREFIX \
       -D CMAKE_BUILD_TYPE=Release \
       -D CMAKE_PREFIX_PATH:FILEPATH=$PREFIX \
       -D CMAKE_SYSTEM_PREFIX_PATH:FILEPATH=$PREFIX \
-      -D CMAKE_CXX_STANDARD=17 \
+      -D CMAKE_CXX_STANDARD=${cpp_std} \
       -D ENABLE_MED:BOOL=OFF \
       -D ENABLE_NETGEN:BOOL=ON \
       ..
