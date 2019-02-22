5e7ef67dbfb0d8baa74e077ae5b5cff8ef373078
lorenz <looooo@users.noreply.github.com>
2019-02-22 08:45:37 +0100

    use CMAKE_POLICY_DEFAULT_CMP0025=NEW

--- recipe/build.sh
+++ recipe/build.sh
@@ -15,6 +15,7 @@ cmake -G "Ninja" \
       -D CMAKE_CXX_STANDARD=${cpp_std} \
       -D ENABLE_MED:BOOL=OFF \
       -D ENABLE_NETGEN:BOOL=ON \
+      -D CMAKE_POLICY_DEFAULT_CMP0025=NEW \
       ..
 
 ninja install -v
