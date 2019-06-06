df029bba8ddddcd7ae63d6216fc8755fde824e61
looooo <sppedflyer@gmail.com>
2019-06-06 18:23:20 +0200

    new patch

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,18 +1,16 @@
 mkdir -p build
 cd build
 
-if [ `uname` = "Darwin" ]; then
-      cpp_std=14
-else
-      cpp_std=17
-fi
+# if [ `uname` = "Darwin" ]; then
+#       cpp_std=14
+# else
+#       cpp_std=17
+# fi
 
 cmake -G "Ninja" \
+      -D CMAKE_BUILD_TYPE:STRING="Release" \
       -D CMAKE_INSTALL_PREFIX:FILEPATH=$PREFIX \
-      -D CMAKE_BUILD_TYPE=Release \
       -D CMAKE_PREFIX_PATH:FILEPATH=$PREFIX \
-      -D CMAKE_SYSTEM_PREFIX_PATH:FILEPATH=$PREFIX \
-      -D CMAKE_CXX_STANDARD=${cpp_std} \
       -D ENABLE_MED:BOOL=OFF \
       -D ENABLE_NETGEN:BOOL=ON \
       ..
