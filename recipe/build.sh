590ef76fe6e13c387b6017c3f5d6498bc6a6c282
looooo <sppedflyer@gmail.com>
2019-06-06 21:23:59 +0200

    workaround for boost-include-dir

--- recipe/build.sh
+++ recipe/build.sh
@@ -11,7 +11,7 @@ cmake -G "Ninja" \
       -D CMAKE_BUILD_TYPE:STRING="Release" \
       -D CMAKE_INSTALL_PREFIX:FILEPATH=$PREFIX \
       -D CMAKE_PREFIX_PATH:FILEPATH=$PREFIX \
-      -D Boost_INCLUDE_DIR:FILEPATH=$PREFIX/include/boost \
+      -D Boost_NO_BOOST_CMAKE:BOOL=ON \
       -D ENABLE_MED:BOOL=OFF \
       -D ENABLE_NETGEN:BOOL=ON \
       ..
