56fc5057296d6fb73c0fe2505346c30010d501f5
looooo <sppedflyer@gmail.com>
2019-06-06 18:26:14 +0200

    delete patch add boost include dir

--- recipe/build.sh
+++ recipe/build.sh
@@ -11,6 +11,7 @@ cmake -G "Ninja" \
       -D CMAKE_BUILD_TYPE:STRING="Release" \
       -D CMAKE_INSTALL_PREFIX:FILEPATH=$PREFIX \
       -D CMAKE_PREFIX_PATH:FILEPATH=$PREFIX \
+      -D Boost_INCLUDE_DIRS:FILEPATH=$PREFIX/include/boost \
       -D ENABLE_MED:BOOL=OFF \
       -D ENABLE_NETGEN:BOOL=ON \
       ..
