677ca43c0807c5fa357ac1dbb85f1951a4370614
looooo <sppedflyer@gmail.com>
2020-05-02 21:24:50 +0200

    netgen 6.2.2004

--- recipe/build.sh
+++ recipe/build.sh
@@ -17,6 +17,8 @@ cmake -G "Ninja" \
       -D CMAKE_CXX_STANDARD=${cpp_std} \
       -D ENABLE_MED:BOOL=OFF \
       -D ENABLE_NETGEN:BOOL=ON \
+      -D CMAKE_CXX_FLAGS="-Wno-deprecated-declarations" \
+      -D NEW_NETGEN_INTERFACE:BOOL=ON \
       ..
 
 ninja install
