12d78ee36279c366291dca294079647a82a6dc18
looooo <sppedflyer@gmail.com>
2020-08-12 12:51:52 +0200

    update

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,16 +2,15 @@ mkdir -p build
 cd build
 
 if [ `uname` = "Darwin" ]; then
+      cpp_std=14
+      # we need to link this because smesh depends on libraries build against different sdk-versions
       ln -s /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.12.sdk /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.9.sdk
       ln -s /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.12.sdk /Applications/Xcode_11.3.1.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.9.sdk
-      cpp_std=14
 else
       cpp_std=17
-      sed -i 's#/home/conda/feedstock_root/build_artifacts/vtk_.*_build_env/x86_64-conda_cos6-linux-gnu/sysroot/usr/include##g' ${PREFIX}/lib/cmake/vtk-9.0/VTK-targets-release.cmake
-      sed -i 's#/home/conda/feedstock_root/build_artifacts/vtk_.*_build_env/x86_64-conda_cos6-linux-gnu/sysroot/usr/lib.*;##g' ${PREFIX}/lib/cmake/vtk-9.0/VTK-targets-release.cmake
-      sed -i 's#/home/conda/feedstock_root/build_artifacts/vtk_.*_build_env/x86_64-conda_cos6-linux-gnu/sysroot/usr/lib.*;##g' ${PREFIX}/lib/cmake/vtk-9.0/VTK-targets.cmake
-      sed -i '381d' ${PREFIX}/lib/cmake/vtk-9.0/VTK-targets.cmake
-      sed -i '383d' ${PREFIX}/lib/cmake/vtk-9.0/VTK-targets.cmake
+      # there are some paths not working in vtk and netcdf
+      sed -i 's#/home/conda/feedstock_root/build_artifacts/vtk_.*_build_env/x86_64-conda_cos6-linux-gnu/sysroot/usr/include##g' ${PREFIX}/lib/cmake/vtk-9.0/VTK*
+      sed -i 's#/home/conda/feedstock_root/build_artifacts/vtk_.*_build_env/x86_64-conda_cos6-linux-gnu/sysroot/usr/lib.*;##g' ${PREFIX}/lib/cmake/vtk-9.0/VTK*
 fi
 
 cmake -G "Ninja" \
