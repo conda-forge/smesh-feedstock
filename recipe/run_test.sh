bdf1417b5194e27e037a0a44d872e055c9e1b2c8
lorenz <looooo@users.noreply.github.com>
2021-05-14 14:54:35 +0200

    Update run_test.sh

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -2,14 +2,8 @@ cd SMESH/test
 mkdir build
 cd build
 
-declare -a CMAKE_PLATFORM_FLAGS
-if [[ ${HOST} =~ .*linux.* ]]; then
-    CMAKE_PLATFORM_FLAGS+=(-DCMAKE_TOOLCHAIN_FILE="${RECIPE_DIR}/cross-linux.cmake")
-fi
-
 cmake -G "Ninja" \
       -D CMAKE_BUILD_TYPE:STRING="Release" \
-      ${CMAKE_PLATFORM_FLAGS[@]} \
       -D CMAKE_INSTALL_PREFIX:FILEPATH=$PREFIX \
       -D CMAKE_PREFIX_PATH:FILEPATH=$PREFIX \
       ..
