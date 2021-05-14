088920ff9a9d42f843e988f90b9449798b6d8827
looooo <sppedflyer@gmail.com>
2021-05-14 12:53:35 +0200

    update tests

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -2,15 +2,27 @@ cd SMESH/test
 mkdir build
 cd build
 
+declare -a CMAKE_PLATFORM_FLAGS
+if [[ ${HOST} =~ .*linux.* ]]; then
+    CMAKE_PLATFORM_FLAGS+=(-DCMAKE_TOOLCHAIN_FILE="${RECIPE_DIR}/cross-linux.cmake")
+fi
+
 cmake -G "Ninja" \
       -D CMAKE_BUILD_TYPE:STRING="Release" \
+      ${CMAKE_PLATFORM_FLAGS[@]} \
       -D CMAKE_INSTALL_PREFIX:FILEPATH=$PREFIX \
       -D CMAKE_PREFIX_PATH:FILEPATH=$PREFIX \
-      -D CMAKE_CXX_STANDARD=14 \
       ..
 
 ninja install
 
 cd ..
 cd tests
-./test_SMESH
+./test_Catch
+if [[ ${HOST} =~ .*apple.* ]]
+then
+    echo "Skipping SMESH tests on OSX until they are fixed..."
+else
+    ./test_StdMeshers
+    ./test_NETGENPlugin
+fi
