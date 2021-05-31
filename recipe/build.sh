cdc61cc6a29e9ab2729fea832d969e3e5fbc7400
lorenz <looooo@users.noreply.github.com>
2021-05-31 15:26:59 +0200

    try remove cmake args

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,7 +6,7 @@ python prepare.py
 mkdir build
 cd build
 
-cmake ${CMAKE_ARGS} -G "Ninja" \
+cmake -G "Ninja" \
       -D CMAKE_BUILD_TYPE:STRING="Release" \
       -D CMAKE_INSTALL_PREFIX:FILEPATH=$PREFIX \
       -D CMAKE_PREFIX_PATH:FILEPATH=$PREFIX \
