aff8a23ed049bf9fadc79efceb85ec1eb95aed92
looooo <sppedflyer@gmail.com>
2021-05-31 11:44:20 +0200

    add mefisto-test

--- recipe/run_test.bat
+++ recipe/run_test.bat
@@ -18,4 +18,5 @@ cd ..
 cd tests
 test_Catch.exe
 test_StdMeshers.exe
-test_NETGENPlugin.exe 
+test_NETGENPlugin.exe
+test_MEFISTO2.exe
--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -27,4 +27,5 @@ then
 else
     ./test_StdMeshers
     ./test_NETGENPlugin
+    ./test_MEFISTO2
 fi 
