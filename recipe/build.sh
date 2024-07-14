2ccb711e52d9daa8aac9115d1c78bc4bd554605a
looooo <sppedflyer@gmail.com>
2024-07-14 21:28:17 +0200

    enable netgen

--- recipe/build.sh
+++ recipe/build.sh
@@ -13,7 +13,7 @@ cmake -G "Ninja" \
       -D CMAKE_PREFIX_PATH:FILEPATH=$PREFIX \
       -D Boost_NO_BOOST_CMAKE:BOOL=ON \
       -D ENABLE_MED:BOOL=OFF \
-      -D ENABLE_NETGEN:BOOL=OFF \
+      -D ENABLE_NETGEN:BOOL=ON \
       ..
 
 ninja install
