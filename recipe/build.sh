edc9f23d068b1c5f07f5b71d6121fe26c91f0c39
looooo <sppedflyer@gmail.com>
2024-07-14 21:45:14 +0200

    remove unused med cmake specification

--- recipe/build.sh
+++ recipe/build.sh
@@ -12,8 +12,8 @@ cmake -G "Ninja" \
       -D CMAKE_INSTALL_PREFIX:FILEPATH=$PREFIX \
       -D CMAKE_PREFIX_PATH:FILEPATH=$PREFIX \
       -D Boost_NO_BOOST_CMAKE:BOOL=ON \
-      -D ENABLE_MED:BOOL=OFF \
       -D ENABLE_NETGEN:BOOL=ON \
+      -D CMAKE_CXX_STANDARD=14 \
       ..
 
 ninja install
